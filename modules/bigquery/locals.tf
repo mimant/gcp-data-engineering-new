36bc312c7cb12c02753fba6fbb9457534e871bdd
mimant <mimant10@gmail.com>
2024-08-06 15:45:21 +0000

    local change

--- modules/bigquery/locals.tf
+++ modules/bigquery/locals.tf
@@ -2,13 +2,13 @@ locals {
 
 datasets = {
    for file in fileset(path.module, "modules/resource/datasets/*json") :
-    file => ( jsondecode(file("${path.module}/${file}")))
+    file => ( jsondecode(file("modules/resource/datasets/${file}")))
 
  }
 
 tables = {
    for file in fileset(path.module, "modules/resource/tables/*json") :
-    file => ( jsondecode(file("${path.module}/${file}")))
+    file => ( jsondecode(file("modules/resource/datasets/${file}")))
 
  }
 }
